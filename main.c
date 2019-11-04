4d616f0aa6b9c79e32ccd616e3d26c714ac36f78
Bgvas <Bgvas@Bgvas-PC>
2019-11-04 23:23:22 +0200

    Added Functions for RandomNumbers and Random Queue Selection

--- main.c
+++ main.c
@@ -33,9 +33,6 @@ int main() {
 
         main_counter++;
     }
-
-
-
 }
 
 // Procedure for 10%
@@ -50,5 +47,28 @@ int chance10(){
     if(n<chance){
         printf("10%%\n");
     }
+}
+
+// Procedure for random numbers
+int randomNumber(){
+	srand(time(NULL));		//Connect random engine with time,
+	int randomnumber;		//to generate real random numbers
+	randomnumber = rand()%10 + 1;	// generate random numbers (1-10)
+	return randomnumber;
+}
+
+// Procedure for Queue random selection
+int randomQueue(){
+	srand(time(NULL));
+	int randomq = rand() % 10 + 1;
+	if(randomq < 4){	// select Queue 1 for 30%
+		return 1;
+	}
+	else if(randomq > 4 && randomq < 6 ){ // select Queue 2 for 20%
+		return 2;
+	}
+	else{			// select no Queue for remain 50%
+		return 0;
+	}
 
 }

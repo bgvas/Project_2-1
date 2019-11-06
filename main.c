24dc9d2fe4b0f944a72ced4931dad4c5a61acb40
Bgvas <Bgvas@Bgvas-PC>
2019-11-06 22:05:51 +0200

    Some corrections

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ struct process{
 	int id;
 	int size;
 	int strtime;
-	char [10] condition;
+	char condition [10];
 	int runtime;
 	struct process *next;
 };
@@ -59,7 +59,7 @@ int chance10(){
     }
 }
 
-// Procedure for random numbers
+// Function for random numbers
 int randomNumber(){
 	srand(time(NULL));		//Connect random engine with time,
 	int randomnumber;		//to generate real random numbers
@@ -67,14 +67,14 @@ int randomNumber(){
 	return randomnumber;
 }
 
-// Procedure for Queue random selection
+// Function for Queue random selection
 int randomQueue(){
 	srand(time(NULL));
 	int randomq = rand() % 10 + 1;
 	if(randomq < 4){	// select Queue 1 for 30%
 		return 1;
 	}
-	else if(randomq > 4 && randomq < 6 ){ // select Queue 2 for 20%
+	else if(randomq > 3 && randomq < 6 ){ // select Queue 2 for 20%
 		return 2;
 	}
 	else{			// select no Queue for remain 50%
